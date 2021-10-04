111fd3e515374688f6a8e137441bf7b286fff7b5
lngthvinh <ltvnth@gmail.com>
2021-10-04 21:20:35 +0700

    Ghi chú Commit

---
+++ 2021/Web/1000 - PHP no jutsu/source/index.php
@@ -0,0 +1,11 @@
+ 
+<<link rel="shortcut icon" href="logo.png" type="image/x-icon">
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Login Page</title>
+<form class="box" action="#" method="GET">
+  <h1>Welcome to my world </h1>
+  <h3>Tell me the password and get the scroll:</h3>
+  <input type="password" name="password" placeholder="Password">
+  <input type="submit" value="Login" />
+ </form>
+<!--hint.txt-->
